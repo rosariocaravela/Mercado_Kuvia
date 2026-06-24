52ee4afa8411fb5c973b044dfe6759fc8aa07b4e
Rosário Pompilio Caravela <rosariocaravela116@gmail.com>
2026-06-24 22:17:39 +0200

    ATUALIZANDO

--- kuvia-frontend/src/services/api.js
+++ kuvia-frontend/src/services/api.js
@@ -2,9 +2,6 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
   timeout: 10000,
 });
 
@@ -14,6 +11,10 @@ api.interceptors.request.use((config) => {
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  if (config.data instanceof FormData) {
+    delete config.headers['Content-Type'];
   }
 
   return config;
